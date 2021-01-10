c18be594bc09cb1c9479d81695175e250cbf06a7
marcuses101 <mnjconnolly@gmail.com>
2021-01-10 17:12:53 -0500

    folder option

--- src/rtestCLI.js
+++ src/rtestCLI.js
@@ -1,18 +1,42 @@
-import fs from 'fs/promises'
-import path from 'path'
+import fs from "fs/promises";
+import fsSync from "fs";
+import path from "path";
 import testTemplate from "./testTemplate";
 
-export async function cli(){
+export async function cli() {
   try {
     const arg = process.argv[2];
-    const componentName = path.basename(arg,'.js');
-    const testPath = path.dirname(arg) + '/'  + componentName +'.test.js'
-    const readFile = await fs.readFile(arg,'utf-8');
-    const isRouterTest = readFile.includes('react-router-dom')
-    const testFile = testTemplate(componentName,isRouterTest)
-    await fs.writeFile(testPath,testFile);
-    console.log(componentName + ' test created')
+    const isDirectory =
+      fsSync.existsSync(arg) && (await fs.lstat(arg)).isDirectory();
+    console.log({ isDirectory });
+    if (isDirectory) {
+      const fileNames = await fs.readdir(arg);
+      const jsFiles = fileNames.filter(
+        (filename) => filename.slice(-3) === ".js"
+      );
+      console.log(jsFiles);
+      await Promise.all(
+        jsFiles.map(async (fileName) => {
+          const componentName = path.basename(fileName, ".js");
+          const testPath = arg + "/" + componentName + ".test.js";
+          const filePath = arg + "/" + fileName;
+          const readFile = await fs.readFile(filePath, "utf-8");
+          const isRouterTest = readFile.includes("react-router-dom");
+          const testFile = testTemplate(componentName, isRouterTest);
+          await fs.writeFile(testPath, testFile);
+        })
+      );
+      return;
+    }
+
+    const componentName = path.basename(arg, ".js");
+    const testPath = path.dirname(arg) + "/" + componentName + ".test.js";
+    const readFile = await fs.readFile(arg, "utf-8");
+    const isRouterTest = readFile.includes("react-router-dom");
+    const testFile = testTemplate(componentName, isRouterTest);
+    await fs.writeFile(testPath, testFile);
+    console.log(componentName + " test created");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
